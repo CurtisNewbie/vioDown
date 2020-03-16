0c0ff049f68a58960c38d0ae012410ce022c5dbf
CurtisNewbie <zhuangyongj@gmail.com>
2020-03-16 21:58:46 +0000

    Seperate audio/video dowloading & merge them with ffmpeg
    As the quality is set to "highestvideo", the downloaded media files often
    do not contain audio in it. In some cases, we have to download the audio
    part individually as well.
    
    Seperating the downloading for video part and audio part makes sense,
    as we cannot verify whether the media file actually has audio. Once the
    audio part and video part are both downloaded, they are merged using
    ffmpeg.

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const readline = require("readline");
+const path = require("path");
 const fileSys = require("fs");
 const ytdl = require("ytdl-core");
+const ffmpeg = require("fluent-ffmpeg");
 
 const reader = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+const AUDIO_INFIX = "_audio";
+const VIDEO_INFIX = "_video";
 
+/**
+ * Callback function for displaying progress
+ */
 function displayProgress(chunkLength, downloaded, total) {
   console.log(
     `${((downloaded / total) * 100).toFixed(2)}% _ Remaining: ${(
@@ -16,6 +23,11 @@ function displayProgress(chunkLength, downloaded, total) {
   );
 }
 
+/**
+ * Download media file on youtube
+ * @param {*} url youtube url
+ * @param {*} filePath absolute or relative path to where the media file should be downloaded, this path should end with a filename without file extension. The downloaded media file will always be of mp4 format
+ */
 function download(url, filePath) {
   if (url) {
     if (!filePath) {
@@ -25,41 +37,109 @@ function download(url, filePath) {
         `Filepath is not specified, it has been changed to \"${filePath}\"`
       );
     }
-
-    console.log(`Downloading '${url}' to '${filePath}'`);
-    ytdl(url, {
-      // change to 'highestaudio' if necessary
-      quality: "highestvideo"
-    })
-      .on("error", console.error)
-      .on("progress", displayProgress)
-      .pipe(fileSys.createWriteStream(filePath))
-      .on("finish", () => {
-        console.log(`Done! Video downloaded - '${filePath}'`);
-      });
+    downloadVideo(url, filePath);
   } else {
     if (!url) console.log("URL cannot be empty");
   }
 }
 
-/*
+/**
+ * Download video part, once it's finished, it calls downloadAudio() method
+ */
+function downloadVideo(url, filePath) {
+  console.log("Downloading Video to", filePath + VIDEO_INFIX);
+  ytdl(url, {
+    quality: "highestvideo",
+    filter: "videoonly"
+  })
+    .on("error", console.error)
+    .on("progress", displayProgress)
+    .pipe(fileSys.createWriteStream(filePath + VIDEO_INFIX))
+    .on("finish", () => {
+      console.log(`Done! Video downloaded - '${filePath + VIDEO_INFIX}'`);
+      downloadAudio(url, filePath);
+    });
+}
+
+/**
+ *  Download audio part, once it's finished, it calls mergeVideoAndAudio()
+ */
+function downloadAudio(url, filePath) {
+  console.log("Downloading Audio to", filePath + AUDIO_INFIX);
+  ytdl(url, {
+    quality: "highestaudio",
+    filter: "audioonly"
+  })
+    .on("error", console.error)
+    .on("progress", displayProgress)
+    .pipe(fileSys.createWriteStream(filePath + AUDIO_INFIX))
+    .on("finish", () => {
+      mergeVideoAndAudio(filePath);
+      console.log(`Done! Audio downloaded - '${filePath + AUDIO_INFIX}'`);
+    });
+}
+
+/**
+ * Merge the video part and audio part using ffmpeg, once finished, it calls deleteTempFiles() to handle
+ * temporary files.
+ */
+function mergeVideoAndAudio(filePath) {
+  let videoPath = filePath + VIDEO_INFIX;
+  let audioPath = filePath + AUDIO_INFIX;
+  let audio = path.resolve(videoPath);
+  let video = path.resolve(audioPath);
+
+  if (!filePath.endsWith(".mp4")) filePath += ".mp4";
+  console.log(`Merging ${videoPath} and ${audioPath} to ${filePath}`);
+
+  ffmpeg()
+    .input(video)
+    .videoCodec("copy")
+    .input(audio)
+    .save(filePath)
+    .on("error", console.error)
+    .on("end", () => {
+      console.log(`Done! Merged File downloaded - ${filePath}`);
+      deleteTempFiles(videoPath, audioPath);
+    });
+}
+
+/**
+ * Delete temporary files
+ * @param {*} pathToVideo path to the video part
+ * @param {*} pathToAudio path to the audio part
+ */
+function deleteTempFiles(pathToVideo, pathToAudio) {
+  console.log(`Deleting '${pathToVideo}' and '${pathToAudio}'`);
+  fileSys.unlinkSync(pathToVideo);
+  fileSys.unlinkSync(pathToAudio);
+  console.log("Finished");
+}
+
+/**
 -----------------------------------------------
 
-Main
+This program always downloads the media file of the highest quality. The video and audio  are 
+downloaded seperately. The general logic is that it starts with downloading video part, then 
+the audio part, and then merge them into a media file of mp4 format. The video part and audio part
+downloaded previously are deleted once they are merged.
 
 -----------------------------------------------
 */
-let url = "";
-let filePath = "";
-console.log();
-reader.question("Enter the video url that your want to download:\n", str => {
-  url = str;
-  reader.question(
-    "Enter the absolute (or relative) path and name of this downloaded file:\n",
-    path => {
-      filePath = path;
-      download(url, filePath);
-      reader.close();
-    }
-  );
-});
+function main() {
+  let url = "";
+  let filePath = "";
+  console.log();
+  reader.question("Enter the video url that your want to download:\n", str => {
+    url = str;
+    reader.question(
+      "Enter the absolute (or relative) path and name of this downloaded file: (it will always be downloaded as mp4 file)\n",
+      path => {
+        filePath = path;
+        download(url, filePath);
+        reader.close();
+      }
+    );
+  });
+}
+main();
