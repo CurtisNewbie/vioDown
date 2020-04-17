1ee83c223cbf56172ada6fdcc040a049d550fe33
CurtisNewbie <zhuangyongj@gmail.com>
2020-04-17 16:25:33 +0800

    Adopt Promise for readability

--- script.js
+++ script.js
@@ -6,29 +6,28 @@ const ffmpeg = require("fluent-ffmpeg");
 
 const reader = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 const AUDIO_INFIX = "_audio";
 const VIDEO_INFIX = "_video";
 
 let start_time;
 let end_time;
+let url;
+let filePath;
 
 let audioProgress = {
   name: "Audio Progress:",
   percentage: "0.00",
   remaining: "",
   chunkLength: "",
-  downloaded: false
 };
 let videoProgress = {
   name: "Video Progress:",
   percentage: "0.00",
   remaining: "",
   chunkLength: "",
-  dowloaded: false
 };
-let isMerging = false;
 
 /**
  * Callback function for updating audio progress
@@ -85,7 +84,8 @@ function display(name, percentage, remaining, chunkLength) {
  * @param {*} url youtube url
  * @param {*} filePath absolute or relative path to where the media file should be downloaded, this path should end with a filename without file extension. The downloaded media file will always be of mp4 format
  */
-function download(url, filePath) {
+function download() {
+  setStartTime();
   if (url) {
     if (!filePath) {
       filePath =
@@ -94,8 +94,9 @@ function download(url, filePath) {
         `Filepath is not specified, it has been changed to \"${filePath}\"`
       );
     }
-    downloadVideo(url, filePath);
-    downloadAudio(url, filePath);
+    Promise.all([downloadVideo(url, filePath), downloadAudio(url, filePath)])
+      .then((values) => mergeVideoAndAudio(values[0], values[1]))
+      .catch(console.log);
   } else {
     if (!url) console.log("URL cannot be empty");
   }
@@ -104,49 +105,46 @@ function download(url, filePath) {
 /**
  * Download video part asynchronously
  */
-function downloadVideo(url, filePath) {
-  let videoPath = filePath + VIDEO_INFIX;
-  ytdl(url, {
-    quality: "highestvideo",
-    filter: "videoonly"
-  })
-    .on("error", console.error)
-    .on("progress", updateVideoProgress)
-    .pipe(fileSys.createWriteStream(videoPath))
-    .on("finish", () => {
-      videoProgress.dowloaded = true;
-      if (!isMerging) mergeVideoAndAudio(filePath);
-    });
+function downloadVideo() {
+  return new Promise((resolve) => {
+    let videoPath = filePath + VIDEO_INFIX;
+    ytdl(url, {
+      quality: "highestvideo",
+      filter: "videoonly",
+    })
+      .on("error", console.error)
+      .on("progress", updateVideoProgress)
+      .pipe(fileSys.createWriteStream(videoPath))
+      .on("finish", () => {
+        resolve(videoPath);
+      });
+  });
 }
 
 /**
  *  Download audio part asynchronously
  */
-function downloadAudio(url, filePath) {
-  let audioPath = filePath + AUDIO_INFIX;
-  ytdl(url, {
-    quality: "highestaudio",
-    filter: "audioonly"
-  })
-    .on("error", console.error)
-    .on("progress", updateAudioProgress)
-    .pipe(fileSys.createWriteStream(audioPath))
-    .on("finish", () => {
-      audioProgress.downloaded = true;
-      if (!isMerging) mergeVideoAndAudio(filePath);
-    });
+function downloadAudio() {
+  return new Promise((resolve) => {
+    let audioPath = filePath + AUDIO_INFIX;
+    ytdl(url, {
+      quality: "highestaudio",
+      filter: "audioonly",
+    })
+      .on("error", console.error)
+      .on("progress", updateAudioProgress)
+      .pipe(fileSys.createWriteStream(audioPath))
+      .on("finish", () => {
+        resolve(audioPath);
+      });
+  });
 }
 
 /**
  * Merge the video part and audio part using ffmpeg, once finished, it calls deleteTempFiles() to handle
  * temporary files.
  */
-function mergeVideoAndAudio(filePath) {
-  if (!audioProgress.downloaded || !videoProgress.dowloaded) return;
-
-  isMerging = true;
-  let videoPath = filePath + VIDEO_INFIX;
-  let audioPath = filePath + AUDIO_INFIX;
+function mergeVideoAndAudio(videoPath, audioPath) {
   let audio = path.resolve(videoPath);
   let video = path.resolve(audioPath);
 
@@ -221,26 +219,22 @@ function displayTimeTook() {
 -----------------------------------------------
 
 This program always downloads the media file of the highest quality. The video and audio are 
-downloaded seperately in parallel. The general logic is that it starts with downloading both 
+downloaded asynchronously using Promise. The general logic is that it starts with downloading both 
 the video part and the audio part asynchronously, and then merge them into a media file of 
 mp4 format. The video part and audio part downloaded previously are deleted once they are merged.
 
 -----------------------------------------------
 */
 function main() {
-  let url = "";
-  let filePath = "";
-  console.log();
   reader.question(
     ">>> Enter the video url that your want to download:\n",
-    str => {
+    (str) => {
       url = str;
       reader.question(
         "\n>>> Enter the absolute or relative path (including file name) to the downloaded file:\n",
-        path => {
+        (path) => {
           filePath = path;
-          setStartTime();
-          download(url, filePath);
+          download();
           reader.close();
         }
       );
